628fefe3263b38090b4f53c0a4b37d6a28ce596c
baliddeki <baliddeki@gmail.com>
2023-10-30 13:44:31 +0300

    example code

---
+++ example.py
@@ -0,0 +1,41 @@
+
+name = "Nakinga Robertta Mbabazi"
+
+var = 0
+
+for a in name:
+    if a == 'a':
+        var = var + 1
+
+print(var)
+
+name = "Nankinga"
+
+# slicing
+
+if 'n' in name[0:4]:
+    print("There is an n in the first four letters")
+
+name = "ALIDDEKI MULINDWA BRYAN"
+ 
+
+# convert to uppercase
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+
+# removing whitespace between strings
+name = "  ALIDDEKI MULINDWA BRYAN  "
+number = " +256 7121 245 345 "
+print(number.strip())
+
+
+
+
+
+
+
+
+
+
+
