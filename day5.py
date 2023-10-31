d14c0c5d9678ddc0d66c3685dba768922ed689d1
baliddeki <baliddeki@gmail.com>
2023-10-31 16:11:50 +0300

    lesson code

--- day5.py
+++ day5.py
@@ -26,7 +26,7 @@ while (count < len(my_list)):
 
 print(list_int)'''
 
-my_list = [1, 2, 3, 'Adam', 'Bright', 4, 5]
+'''my_list = [1, 2, 3, 'Adam', 'Bright', 4, 5]
 my_list.insert(4, 6)
 print(my_list)
 print(my_list[1:4])
@@ -35,7 +35,35 @@ print(my_list[1:4])
 # 1. Create a list of 5 elements
 # 2. Add 2 more elements to the list
 # 3. Remove the 3rd element from the list
-# 4. Print the list
+# 4. Print the list'''
+
+'''nums = [1, 2, 3, 4]
+names = ['micheal', 'micheal', 'sam']
+names.extend(nums)
+combined_list = nums.extend(names)
+# an_combined_list = nums + names
+print(names)
+# print(nums)
+# print(combined_list)
+# print(an_combined_list)'''
+
+
+'''num_list = [1, 2, 3, 4, 5]
+num_list.pop(2) # for pop method you specify the index
+print(num_list )'''
+
+num_list = [1, 2, 3, 4, 5]
+'''
+
+# remove method - specify the item
+# clear - empty the list
+
+print(num_list)'''
+
+copy_list = num_list.copy()
+copy_list.sort(reverse=True)
+print(copy_list)
+
 
 
 
