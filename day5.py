49b906e6b49cc1a278a549ba657280cf223817de
baliddeki <baliddeki@gmail.com>
2023-10-31 15:39:04 +0300

    lesson on data structures; lists and tuples

---
+++ day5.py
@@ -0,0 +1,46 @@
+# Python Lists
+
+'''my_list = ["Betty", "Samson", "Brian"]
+print(type(my_list))'''
+
+'''my_list = list()
+print(type(my_list))
+my_list.append("Betty")
+my_list.append("Samson")
+my_list.append("Brian")
+
+print(len(my_list))'''
+
+
+
+'''count = 0
+list_int = list()
+
+while (count < len(my_list)):
+# for element in my_list:
+    if (type(my_list[count]) == int):
+            list_int.append(my_list[count])
+    count += 1
+    
+
+
+print(list_int)'''
+
+my_list = [1, 2, 3, 'Adam', 'Bright', 4, 5]
+my_list.insert(4, 6)
+print(my_list)
+print(my_list[1:4])
+
+# Simple Exercise on list methods
+# 1. Create a list of 5 elements
+# 2. Add 2 more elements to the list
+# 3. Remove the 3rd element from the list
+# 4. Print the list
+
+
+
+
+
+
+
+
